dc2020d909e2931b332664cd8e7fd32beb9fe225
Cristian Garcia <crisg@sas.upenn.edu>
2023-11-22 13:30:37 -0500

    added a new file and created an App functional component

---
+++ main.jsx
@@ -0,0 +1,11 @@
+// a functional component is a function
+const App = () => {
+    return (
+        <div>
+            <h1>My Ugly React App</h1>
+            <h2>Ugliest Website Ever</h2>
+        </div>
+    );
+};
+
+export default App;
